ba0f61b550211b180ab8bd3061c2c4b6f2d8628d
Nat Welch <nat@natwelch.com>
2011-09-24 21:20:44 -0700

    Playing with GitHub authentication.

--- site.rb
+++ site.rb
@@ -22,6 +22,23 @@ post '/' do
    redirect '/'
 end
 
+post '/commit' do
+  p params
+end
+
+
+## Oauth Stuff for GitHub
+get '/login' do
+  client = '27affb525ae9efd596a0'
+  redirect "https://github.com/login/oauth/authorize?client_id=#{client}"
+end
+
+get '/authed' do
+  p params
+end
+
+
+## Style sheet
 get '/style.css' do
    content_type 'text/css', :charset => 'utf-8'
    less :style
