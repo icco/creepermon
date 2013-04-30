609aff44e167c8e4a27cf0aafbaa664e1f865443
Nat Welch <nat@natwelch.com>
2013-04-30 01:23:42 -0700

    fix migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 7) do
+ActiveRecord::Schema.define(:version => 8) do
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "target"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
