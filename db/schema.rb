dfc37014920d05235a1cfc8e7622c41421adf641
Nat Welch <nat@natwelch.com>
2013-04-30 01:18:44 -0700

    redo users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 6) do
-
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.text     "sites"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 7) do
 
 end
