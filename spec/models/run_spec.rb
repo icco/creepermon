ebeac05bb89f70f1bb37c5d6b9843e5c2cd75abf
Nat Welch <nat@natwelch.com>
2013-04-30 00:15:12 -0700

    not needed.

--- spec/models/run_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'spec_helper'
-
-describe Run do
-end
--- spec/models/site_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe "Site Model" do
-  let(:site) { Site.new }
-  it 'can be created' do
-    site.should_not be_nil
-  end
-end
