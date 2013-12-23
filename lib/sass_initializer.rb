2b15cd2128db328c3d265af363785a8abfadc04c
Nat Welch <nat@natwelch.com>
2013-12-23 14:53:54 -0800

    change scss config

--- lib/sass_initializer.rb
+++ lib/sass_initializer.rb
@@ -2,10 +2,14 @@ module SassInitializer
   def self.registered(app)
     # Enables support for SASS template reloading in rack applications.
     # See http://nex-3.com/posts/88-sass-supports-rack for more details.
-    # Store SASS files (by default) within 'app/css'.
+    # http://sass-lang.com/documentation/file.SASS_REFERENCE.html#options
     require 'sass/plugin/rack'
     Sass::Plugin.options[:template_location] = Padrino.root("app/css")
     Sass::Plugin.options[:css_location] = Padrino.root("public/css")
+    Sass::Plugin.options[:cache] = true
+    Sass::Plugin.options[:always_update] = true if app.development?
+    Sass::Plugin.options[:style] = :compressed
+
     app.use Sass::Plugin::Rack
   end
 end
