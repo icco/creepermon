6f7a679f5436cb844e4226d141ca339d323992e1
Nat Welch <nat@natwelch.com>
2013-12-24 15:22:01 -0800

    hmm.

--- app/app.rb
+++ app/app.rb
@@ -13,20 +13,7 @@ module CreeperMon
     #
     register Padrino::Cache
     enable :caching
-
-    # You can customize caching store engines:
-    #
-    # set :cache, Padrino::Cache.new(:LRUHash) # Keeps cached values in memory
-    # set :cache, Padrino::Cache.new(:Memcached) # Uses default server at localhost
-    # set :cache, Padrino::Cache.new(:Memcached, '127.0.0.1:11211', :exception_retry_limit => 1)
-    # set :cache, Padrino::Cache.new(:Memcached, :backend => memcached_or_dalli_instance)
-    # set :cache, Padrino::Cache.new(:Redis) # Uses default server at localhost
-    # set :cache, Padrino::Cache.new(:Redis, :host => '127.0.0.1', :port => 6379, :db => 0)
-    # set :cache, Padrino::Cache.new(:Redis, :backend => redis_instance)
-    # set :cache, Padrino::Cache.new(:Mongo) # Uses default server at localhost
-    # set :cache, Padrino::Cache.new(:Mongo, :backend => mongo_client_instance)
-    # set :cache, Padrino::Cache.new(:File, :dir => Padrino.root('tmp', app_name.to_s, 'cache')) # default choice
-    #
+    set :cache, Padrino::Cache.new(:File, :dir => Padrino.root('tmp', 'cache'))
 
     ##
     # Application configuration options.
@@ -39,8 +26,6 @@ module CreeperMon
     # You can configure for a specified environment like:
     #
     configure :development do
-      set :foo, :bar
-      disable :asset_stamp # no asset timestamping for dev
     end
 
     error 404 do
