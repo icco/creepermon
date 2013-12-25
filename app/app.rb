2f5682fc6f637de71b39828f1fde1d60ed38c6c4
Nat Welch <nat@natwelch.com>
2013-12-24 21:12:44 -0800

    Got basic sprockets working

--- app/app.rb
+++ app/app.rb
@@ -10,11 +10,11 @@ module CreeperMon
     #
     register Padrino::Pipeline
     configure_assets do |config|
-      config.pipeline = Padrino::Pipeline::AssetPack
+      config.pipeline = Padrino::Pipeline::Sprockets
       config.css_prefix = '/css'
-      config.css_assets = '/app/css'
+      config.css_assets = Padrino.root('app', 'css')
       config.js_prefix = '/js'
-      config.js_assets = '/app/js'
+      config.js_assets = Padrino.root('app', 'js')
     end
 
     ##
@@ -33,21 +33,13 @@ module CreeperMon
     # Application configuration options.
     #
     set :logging, true
-    set :js_asset_folder, 'js'
-    set :css_asset_folder, 'css'
+    #set :js_asset_folder, 'js'
+    #set :css_asset_folder, 'css'
 
     ##
     # You can configure for a specified environment like:
     #
     configure :development do
-    end
-
-    error 404 do
-      render 'errors/404'
-    end
-
-    error 505 do
-      render 'errors/505'
     end
   end
 end
--- config/boot.rb
+++ config/boot.rb
@@ -8,28 +8,16 @@ require 'bundler/setup'
 Bundler.require(:default, PADRINO_ENV)
 
 ##
-# ## Enable devel logging
+# Enable devel logging
 #
-# Padrino::Logger::Config[:development][:log_level]  = :devel
-# Padrino::Logger::Config[:development][:log_static] = true
+Padrino::Logger::Config[:development][:log_level]  = :devel
+Padrino::Logger::Config[:development][:log_static] = true
+
+##
+# Configure your I18n
 #
-# ## Configure your I18n
-#
-# I18n.default_locale = :en
-# I18n.enforce_available_locales = false
-#
-# ## Configure your HTML5 data helpers
-#
-# Padrino::Helpers::TagHelpers::DATA_ATTRIBUTES.push(:dialog)
-# text_field :foo, :dialog => true
-# Generates: <input type="text" data-dialog="true" name="foo" />
-#
-# ## Add helpers to mailer
-#
-# Mail::Message.class_eval do
-#   include Padrino::Helpers::NumberHelpers
-#   include Padrino::Helpers::TranslationHelpers
-# end
+I18n.default_locale = :en
+I18n.enforce_available_locales = false
 
 ##
 # Add your before (RE)load hooks here
