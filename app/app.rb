2b337bcb60fe6d33756b4163ff0e3899f4267a69
Nat Welch <nat@natwelch.com>
2013-12-24 17:17:04 -0800

    padrino-pipeline

--- app/app.rb
+++ app/app.rb
@@ -1,35 +1,22 @@
 module CreeperMon
   class App < Padrino::Application
-    set :root, File.dirname(__FILE__)
-    register Sinatra::AssetPack
     use ActiveRecord::ConnectionAdapters::ConnectionManagement
     register Padrino::Rendering
     register Padrino::Mailer
     register Padrino::Helpers
 
+
     ##
     # Asset Compilation
     #
-    assets {
-      serve '/js',     from: 'js'        # Default
-      serve '/css',    from: 'css'       # Default
-      serve '/img',    from: 'img'    # Default
-
-      # The second parameter defines where the compressed version will be served.
-      # (Note: that parameter is optional, AssetPack will figure it out.)
-      js :app, '/js/app.js', [
-        '/js/vendor/**/*.js',
-        '/js/lib/**/*.js'
-      ]
-
-      css :app, '/css/app.css', [
-        '/css/*.css',
-        '/css/*.scss',
-      ]
-
-      js_compression  :uglify
-      css_compression :sass
-    }
+    register Padrino::Pipeline
+    configure_assets do |config|
+      config.pipeline = Padrino::Pipeline::AssetPack
+      config.css_prefix = '/css'
+      config.css_assets = '/app/css'
+      config.js_prefix = '/js'
+      config.js_assets = '/app/js'
+    end
 
     ##
     # Sessions
