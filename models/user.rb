e28d1f8452c693ed194ce0fac611789db9fdd1f3
Nat Welch <nat@natwelch.com>
2014-03-01 14:35:11 -0800

    more old code

---
+++ models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  validates_format_of :name, :with => /^[-a-z0-9_+\.]+$/i
+  validates_uniqueness_of :name
+  validates_format_of :email, :with => /@/
+
+  def self.factory name
+    return User.find_or_create_by_name name
+  end
+end
