888a918f1905bad675891b6f37c5508dde655913
Nat Welch <nat@natwelch.com>
2016-01-30 15:29:08 +0000

    simplest site

---
+++ site.rb
@@ -0,0 +1,33 @@
+RACK_ENV ||= ENV["RACK_ENV"] ||= "development" unless defined?(RACK_ENV)
+
+require "rubygems" unless defined?(Gem)
+require "bundler/setup"
+Bundler.require(:default, RACK_ENV)
+
+require "json"
+require "open-uri"
+require "rss"
+require "set"
+require "logger"
+
+require "./lib/logging.rb"
+require "./lib/scss_init.rb"
+
+class Creepermon < Sinatra::Base
+  register ScssInitializer
+  use Rack::Deflater
+  register Sinatra::ActiveRecordExtension
+
+  layout :main
+
+  configure do
+    enable :caching
+    set :logging, true
+    set :protection, true
+    set :protect_from_csrf, true
+  end
+
+  get "/" do
+    erb :index
+  end
+end
