0518a7370d4fb0863b0a62624d0878946e14b467
Nat Welch <nat@natwelch.com>
2016-01-30 15:26:25 +0000

    we're restarting

--- test/models/site_test.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/../test_config.rb')
-
-describe "Site Model" do
-  it 'can construct a new instance' do
-    @site = Site.new
-    refute_nil @site
-  end
-end
--- test/models/user_test.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/../test_config.rb')
-
-describe "User Model" do
-  it 'can construct a new instance' do
-    @user = User.new
-    refute_nil @user
-  end
-end
